071582db017f4674b7aecc27459590a58bc3a7cc
Mark Rendle <mark@markrendle.net>
2011-06-06 07:56:51 +0100

    Static content tweaks

---
+++ Heckle.App/Bootstrapper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Responses;
+
+namespace Heckle.App
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void InitialiseInternal(TinyIoC.TinyIoCContainer container)
+        {
+            base.InitialiseInternal(container);
+            BeforeRequest += ctx =>
+            {
+                var rootPathProvider =
+                    container.Resolve<IRootPathProvider>();
+
+                var staticFileExtensions =
+                    new Dictionary<string, string>
+                    {
+                        { "jpg", "image/jpeg" },
+                        { "png", "image/png" },
+                        { "gif", "image/gif" },
+                        { "css", "text/css" },
+                        { "js",  "text/javascript" }
+                    };
+
+                var requestedExtension =
+                    Path.GetExtension(ctx.Request.Uri);
+
+                if (!string.IsNullOrEmpty(requestedExtension))
+                {
+                    var extensionWithoutDot =
+                        requestedExtension.Substring(1);
+
+                    if (staticFileExtensions.Keys.Any(x => x.Equals(extensionWithoutDot, StringComparison.InvariantCultureIgnoreCase)))
+                    {
+                        var fileName =
+                            Path.GetFileName(ctx.Request.Uri);
+
+                        if (fileName == null)
+                        {
+                            return null;
+                        }
+
+                        var filePath =
+                            Path.Combine(rootPathProvider.GetRootPath(), "content", fileName);
+
+                        return !File.Exists(filePath) ? null : new GenericFileResponse(filePath, staticFileExtensions[extensionWithoutDot]);
+                    }
+                }
+
+                return null;
+            };
+        }
+    }
+}
---
+++ Heckle.App/MainModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Simple.Data;
+
+namespace Heckle.App
+{
+    public class MainModule : NancyModule
+    {
+        private readonly dynamic _db = Database.OpenNamedConnection("local");
+        public MainModule()
+        {
+            Get["/"] = _ => View["index"];
+
+            Get["/session/{Event}/{Slot}/{Track}"] = req =>
+                                                 {
+                                                     var session = _db.Sessions.FindByEventCodeAndSlotAndTrack((string)req.Event,
+                                                                                                           (int)req.Slot,
+                                                                                                           (int)req.Track);
+                                                     return View["session", session];
+                                                 };
+        }
+    }
+}
